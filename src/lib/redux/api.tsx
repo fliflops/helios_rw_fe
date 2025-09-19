cc085a8ddbf8e252d64ece3e3b741b2fb81a01b2
jomar.furiscal <jomar.furiscal@barapido.com>
2025-09-19 16:07:04 +0800

    handle no message error

--- src/lib/redux/api.tsx
+++ src/lib/redux/api.tsx
@@ -35,7 +35,11 @@ export const errorHandler: Middleware =
             }
         // eslint-disable-next-line @typescript-eslint/no-explicit-any
         }) = action.payload as any;
-        toast.error(payload.data.message)
+        let msg = "Unable to connect to server";
+        if (payload.data && payload.data.message) {
+            msg = payload.data.message
+        }
+        toast.error(msg)
     }
     return next(action)
 } 
