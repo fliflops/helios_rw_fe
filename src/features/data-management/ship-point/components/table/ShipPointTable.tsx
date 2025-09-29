16d5ca310b96a975eb583e7f0e468990e5a855ff
jomar.furiscal <jomar.furiscal@barapido.com>
2025-09-29 12:53:10 +0800

    add search filter

--- src/features/data-management/ship-point/components/table/ShipPointTable.tsx
+++ src/features/data-management/ship-point/components/table/ShipPointTable.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import { ColumnDef } from '@tanstack/react-table';
 import APITable from '@/components/table/APITable';
-import {Button} from '@/components/ui/button';
+import { Button } from '@/components/ui/button';
 import { useNavigate } from 'react-router-dom';
 import { shipPointTable } from '../../types';
+import { useAppDispatch, useAppSelector } from '@/hooks/redux.hooks';
+import { getViewSearch, setViewSearchFilter } from '@/lib/redux/slices/filter.slice';
 
 interface ShipPointTableProps {
 
@@ -11,7 +13,9 @@ interface ShipPointTableProps {
 
 const ShipPointTable: React.FC<ShipPointTableProps> = () => {
     const navigate = useNavigate();
-    
+    const dispatch = useAppDispatch();
+    const filters = useAppSelector(getViewSearch);
+
     const columns: ColumnDef<shipPointTable>[] = [
         {
             header: 'Ship Point ID',
@@ -22,6 +26,10 @@ const ShipPointTable: React.FC<ShipPointTableProps> = () => {
             accessorKey: 'ship_point_desc'
         },
         {
+            header: 'Address',
+            accessorKey: 'ship_point_address'
+        },
+        {
             header: 'Status',
             accessorKey: 'is_active',
             cell: (props) => {
@@ -30,23 +38,31 @@ const ShipPointTable: React.FC<ShipPointTableProps> = () => {
         },
         {
             header: 'Action',
-            cell: (props) =>{
+            cell: (props) => {
                 const value = props.row.original.id
                 const handleUpdate = () => {
                     navigate('/ship-point/' + value)
                 }
                 return <>
-                <Button onClick={handleUpdate}>Edit</Button>
-            </>  
-            } 
+                    <Button onClick={handleUpdate}>Edit</Button>
+                </>
+            }
         }
     ];
+
+    const handleSearch = (search: string) => {
+        dispatch(setViewSearchFilter({
+            ...filters,
+            search
+        }))
+    }
 
     return <>
         <APITable
             columns={columns}
             route='/ship-point'
-            onSearch={()=>{}}
+            onSearch={handleSearch}
+            filters={filters}
         />
     </>;
 }
